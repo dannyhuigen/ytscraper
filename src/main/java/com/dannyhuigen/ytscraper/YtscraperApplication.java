181391317117259b6ca9cd555d9b98ee1e0a1775
danny <hello@dannyhuigen.nl>
2023-05-06 20:04:33 +0200

    danny - inital spring boot setup

---
+++ src/main/java/com/dannyhuigen/ytscraper/YtscraperApplication.java
@@ -0,0 +1,13 @@
+package com.dannyhuigen.ytscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtscraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dannyhuigen/ytscraper/YtscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannyhuigen.ytscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
